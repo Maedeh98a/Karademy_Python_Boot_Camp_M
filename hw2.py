96adab6d259c79597d60a6d1e2268c04ed7759cb
Maedeh98a <maedeh.ahmadian98@gmail.com>
2020-07-16 14:06:19 -0700

    homework for first week

---
+++ hw2.py
@@ -0,0 +1,14 @@
+input_list = input("please input numbers for list :").split(",")
+input_num = int(input("please enter your number :"))
+
+for list_index in range(len(input_list)):
+    temp = input_num - (int(input_list[list_index]))
+    if str(temp) in input_list:
+        print("numbers and index are :\n")
+        print("first number and it's index: ",input_list[list_index],list_index)
+        print("second number and it's index: ",temp, input_list.index(str(temp)))
+        break
+else:
+    print("sorry! don't have two number for you :))")
+    
+    
