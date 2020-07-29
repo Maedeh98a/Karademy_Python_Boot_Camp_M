4b9cdf7736992e01b502754ddf4a45d49b16732d
Maedeh98a <maedeh.ahmadian98@gmail.com>
2020-07-29 13:09:02 -0700

    Edit namings

--- second_week/python_overview.py
+++ second_week/python_overview.py
@@ -1,31 +1,35 @@
 class DetectMail:
     def __init__(self, file):
-        self._filepath = file
+        self._file_path_ = file
 
     def list_file(self):
-        with open(self._filepath) as FILE:
+        with open(self._file_path_) as FILE:
             list_mail = FILE.readlines()
             return list_mail
 
-    def seperate_email(self, all_mail):
+    @staticmethod
+    def separate_email(all_mail):
         gmail_list = list(filter(lambda x: "gmail.com" in x, all_mail))
         for element in gmail_list:
             all_mail.remove(element)
         return gmail_list, all_mail
 
-    def mail_filter(self, gmails):
+    @staticmethod
+    def mail_filter(gmails):
 
         for gmail_element in gmails:
             new_element = str(gmail_element.replace(".", ""))
             tmp = new_element.replace("gmailcom", "gmail.com")
             yield tmp
 
-    def remove_duplicate(self, old_non_gmail, old_gmail):
+    @staticmethod
+    def remove_duplicate(old_non_gmail, old_gmail):
         new_non_gmail = list(dict.fromkeys(old_non_gmail))
         new_gmail = list(dict.fromkeys(old_gmail))
         return new_non_gmail, new_gmail
 
-    def return_output(self, all_mails, all_gmails):
+    @staticmethod
+    def return_output(all_mails, all_gmails):
         mails = all_mails + all_gmails
 
         for mail_element in mails:
@@ -41,7 +45,7 @@ class DetectMail:
 file_input = input("enter the address of file:")
 my_obj = DetectMail(file_input)
 list_all_mail = my_obj.list_file()
-gmail_elements, non_gmail_mail = my_obj.seperate_email(list_all_mail)
+gmail_elements, non_gmail_mail = my_obj.separate_email(list_all_mail)
 filtered_mail = list(my_obj.mail_filter(gmail_elements))
 new_not_gmail, new_gmail = my_obj.remove_duplicate(non_gmail_mail, filtered_mail)
 print(new_gmail)
